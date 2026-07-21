4c19a9086c422b975a0bfc95d9fa2d2ff67acb8f
Daniel <danielkozlowski92@gmail.com>
2026-07-21 21:06:19 +0200

    załączników

--- editor/integration.config.js
+++ editor/integration.config.js
@@ -2,7 +2,7 @@ window.MAGNET_ORDER_INTEGRATION = {
   // "demo" pobiera projekt PNG i dane zamówienia JSON na komputer klienta.
   // Aby wysyłać zamówienia, zmień tryb na "endpoint" i podaj publiczny adres API.
   mode: "formsubmit",
-  formSubmitUrl: "https://formsubmit.co/ajax/kontakt@mistermagnesik.pl",
+  formSubmitUrl: "https://formsubmit.co/kontakt@mistermagnesik.pl",
   endpointUrl: "",
   publicAnonKey: "",
   headers: {}
